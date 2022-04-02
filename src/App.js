cfd335aec0ed1c38ee9d6cb70e7ce78d52485f83
LEO-Malay <malaybhavsar.290@gmail.com>
2022-04-02 13:38:16 +0530

    Life Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
-import {} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+// Views
+import Bingo from "./view/Bingo";
+import Home from "./view/Home";
+import Result from "./view/Result";
+import Techwood from "./view/Techwood";
+// Components
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+
 const App = () => {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Bingo" element={<Bingo />} />
+                <Route path="/Techwood" element={<Techwood />} />
+                <Route path="/Result" element={<Result />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css/index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+// Generalized CSS
+import "./css/index.css";
+import "./css/HeaderFooter.css";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
